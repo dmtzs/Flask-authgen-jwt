37d0f639779df8eba059f7dc3109258f2715f308
Diego Martínez Sánchez <diego.mtzs00@gmail.com>
2022-10-17 19:46:01 -0500

    Update flask_authgen_jwt.py
    Added more functionality and added more doc strings for the functions

--- src/flask_authgen_jwt.py
+++ src/flask_authgen_jwt.py
@@ -11,6 +11,7 @@ This module provides creation of new jwt also using basic auth to get the jwt an
 try:
     import jwt
     import typing
+    import traceback
     from functools import wraps
     from base64 import b64decode
     from flask import request, current_app, abort, make_response, jsonify
@@ -85,18 +86,27 @@ class Core():
             return func
 
 class GenJwt(Core):
-    def __init__(self, default_jwt_claims: bool = True, registered_claims_only: bool = True):
+    def __init__(self, default_jwt_claims: bool = True, registered_claims_only: bool = True, complete_traceback_genjwt: bool = False):
         self.jwt_fields_attr: dict = None
         self.default_jwt_claims: bool = default_jwt_claims
         self.registered_claims_only: bool = registered_claims_only
+        self.complete_traceback_genjwt: bool = complete_traceback_genjwt
     
     def __validate_registered_claims(self):
+        """
+        Method to validate the registered claims if registered_claims_only is True.
+        Cause this means that the user can only use the registered(standard) claims.
+        """
         registered_claims = ["iss", "sub", "aud", "exp", "nbf", "iat", "jti"]
         for claim in self.jwt_fields_attr:
             if claim not in registered_claims:
                 self.gen_abort_error(f"The claim {claim} is not a registered claim", 400)
 
     def __create_jwt_payload(self) -> dict:
+        """
+        Method to create the JWT payload but still not encoded
+        :return: JWT payload as a dictionary
+        """
         if not self.jwt_fields_attr:
             self.gen_abort_error("jwt_claims decorator and function is not defined", 500)
         if self.registered_claims_only:
@@ -113,6 +123,9 @@ class GenJwt(Core):
         return payload
     
     def __verify_basic_auth(self):
+        """
+        Method to decode and verify the basic auth credentials in the expected format
+        """
         auth_header = request.headers.get("Authorization")
         auth_header = auth_header.split(" ")
         if auth_header[0] != "Basic":
@@ -129,6 +142,11 @@ class GenJwt(Core):
             self.gen_abort_error("User or password is not correct", 401)
     
     def __encode_jwt(self, payload) -> tuple[str, None]:
+        """
+        Method to encode the JWT token using the key and algorithm specified in the enc_dec_jwt_config decorator
+        that returns the dictionary with the configuration.
+        :return: the encoded token or None if an error occurred
+        """
         self.verify_dict_config("jwt")
         key = self.enc_dec_jwt_callback["key"]
         algorithm = self.enc_dec_jwt_callback["algorithm"]
@@ -169,15 +187,26 @@ class GenJwt(Core):
         def func_to_receive(func):
             @wraps(func)
             def wrapper(*args, **kwargs):
-                if self.enc_dec_jwt_callback is None:
-                    self.gen_abort_error("get_decode_jwt_attributes decorator and function to verify password and username is not set", 500)
-                else:
-                    self.__verify_basic_auth()
-                    jwt_payload = self.__create_jwt_payload()
-                    token = self.__encode_jwt(jwt_payload)
-                    self.verify_user_roles(roles)
+                try:
+                    if self.enc_dec_jwt_callback is None:
+                        self.gen_abort_error("get_decode_jwt_attributes decorator and function to verify password and username is not set", 500)
+                    else:
+                        self.__verify_basic_auth()
+                        jwt_payload = self.__create_jwt_payload()
+                        token = self.__encode_jwt(jwt_payload)
+                        self.verify_user_roles(roles)
 
-                return self.ensure_sync(func)(token, *args, **kwargs)
+                    return self.ensure_sync(func)(token, *args, **kwargs)
+                except Exception:
+                    if self.complete_traceback_genjwt:
+                        # This can be used to return traceback using the API for DEV purposes.
+                        error = traceback.format_exc()
+                        self.gen_abort_error(str(error), 500)
+                    else:
+                        # Otherwise this is the default which returns an internal server error to the API
+                        # and prints the traceback to the console.
+                        print(f"The following ERROR occurred in {__file__}: {traceback.format_exc()}")
+                        self.gen_abort_error("Internal server error", 500)
             return wrapper
         if func:
             return func_to_receive(func)
@@ -185,11 +214,18 @@ class GenJwt(Core):
 
 class DecJwt(Core):
     token: dict = None
-    def __init__(self):
-        self.get_jwt_claims_to_verify_callback: list = None
+    def __init__(self, token_as_attr: bool = False, complete_traceback_decjwt: bool = False):
+        self.token_as_attr: bool = token_as_attr
         self.credentials_success_callback: dict = None
+        self.get_jwt_claims_to_verify_callback: list = None
+        self.complete_traceback_decjwt: bool = complete_traceback_decjwt
     
     def __decode_jwt(self) -> tuple[str, None]:
+        """
+        Decode the JWT token using the key and algorithm specified in the enc_dec_jwt_config decorator
+        that returns the dictionary with the configuration.
+        :return: the decoded token or None if an error occurred
+        """
         auth_header = request.headers.get("Authorization")
         auth_header = auth_header.split(" ")
         token = auth_header[1]
@@ -219,6 +255,27 @@ class DecJwt(Core):
                         self.gen_abort_error(f"The claim {claim} is not in the token", 400)
             if len(token) < 1:
                 self.gen_abort_error("Invalid token", 401)
+    
+    def __authenticate_credentials(self, token):
+        """
+        Verify the credentials of the user, if the credentials are not correct then the user will be unauthorized
+        :param token: token to verify the credentials
+        """
+        if self.credentials_success_callback is None:
+            self.gen_abort_error("get_credentials_success decorator is not set", 500)
+        else:
+            keys_to_validate = self.get_jwt_claims_to_verify_callback.keys()
+            for key in keys_to_validate:
+                if self.credentials_success_callback[key] != token[key]:
+                    self.gen_abort_error("Credentials to validate for authentication inside token are not correct", 401)
+
+    def __set_token_as_attr(self, token):
+        """
+        Method to set the token as an attribute of the class
+        :param token: token to set as attribute
+        """
+        if self.token_as_attr:
+            self.token = token
 
     def get_jwt_claims_to_verify(self, func) -> typing.Callable:
         """Decorator to get the claims to verify in the token
@@ -242,15 +299,27 @@ class DecJwt(Core):
         def func_to_receive(func):
             @wraps(func)
             def wrapper(*args, **kwargs):
-                if self.enc_dec_jwt_callback is None:
-                    self.gen_abort_error("get_decode_jwt_attributes decorator and function to verify password and username is not set", 500)
-                else:
-                    token = self.__decode_jwt()
-                    self.__verify_token(token)
-                    self.verify_user_roles(roles)
-                    self.token = token
+                try:
+                    if self.enc_dec_jwt_callback is None:
+                        self.gen_abort_error("get_decode_jwt_attributes decorator and function to verify password and username is not set", 500)
+                    else:
+                        token = self.__decode_jwt()
+                        self.__verify_token(token)
+                        self.verify_user_roles(roles)
+                        self.__authenticate_credentials(token)
+                        self.__set_token_as_attr(token)
 
-                return self.ensure_sync(func)(*args, **kwargs)
+                    return self.ensure_sync(func)(*args, **kwargs)
+                except Exception:
+                    error = traceback.format_exc()
+                    if self.complete_traceback_decjwt:
+                        # This can be used to return traceback using the API for DEV purposes.
+                        self.gen_abort_error(str(error), 500)
+                    else:
+                        # Otherwise this is the default which returns an internal server error to the API
+                        # and prints the traceback to the console.
+                        print(f"The following ERROR occurred in {__file__}: {error}")
+                        self.gen_abort_error("Internal server error", 500)
             return wrapper
         if func:
             return func_to_receive(func)
